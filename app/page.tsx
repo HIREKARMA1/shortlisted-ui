8bd14fc6bc84d889f0fd1f28627ba61b550cf526
Lkanath Panda <lokanthpanda128@gmai.com>
2026-01-20 18:00:15 +0530

    why shotlisted page created
    Signed-off-by: Lkanath Panda <lokanthpanda128@gmai.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { HeroSection } from "@/components/home/hero-section"
+import { WhyShortlistedSection } from "@/components/home/why-shortlisted-section"
 
 export default function HomePage() {
     return (
         <main className="bg-white dark:bg-[#1a1f2e] min-h-screen">
             <HeroSection />
+            <WhyShortlistedSection />
         </main>
     )
 }
