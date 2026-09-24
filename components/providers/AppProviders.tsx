2f5fb26e2ad4d74fa45bbf2874a8eb1cba956a5a
Akash kumar sahu <sahuakashkumar915@gmail.com>
2026-09-24 11:12:42 +0530

    added the changes

--- components/providers/AppProviders.tsx
+++ components/providers/AppProviders.tsx
@@ -2,7 +2,6 @@
 
 import { I18nProvider } from '@/lib/i18n/context';
 import { themeToCssVariables } from '@/theme';
-import { WhatsAppFab } from '@/components/ui/WhatsAppFab';
 import { Toaster } from 'react-hot-toast';
 
 export function AppProviders({ children }: { children: React.ReactNode }) {
@@ -11,7 +10,6 @@ export function AppProviders({ children }: { children: React.ReactNode }) {
   return (
     <I18nProvider>
       <div style={cssVars as React.CSSProperties}>{children}</div>
-      <WhatsAppFab />
       <Toaster position="top-right" />
     </I18nProvider>
   );
